3578d2f936fb9801b74e52e74a8189712568a5a9
DanielRealpe <daniel_realpee@soy.sena.edu.co>
2025-02-26 23:36:44 -0500

    Validaciones Customers - GUSs

--- src/models/Customer_Model.js
+++ src/models/Customer_Model.js
@@ -1,38 +1,51 @@
 import { DataTypes } from 'sequelize';
-import {database} from '../config/Database.js';
+import {database} from '../config/database.js';
 
 export const Customers = database.define('Customers', {
-    Id_Customer: {
-        type: DataTypes.INTEGER,
+    idCustomer: {
+        type: DataTypes.INTEGER(),
         primaryKey: true,
         autoIncrement: true,
     },
-    Name: {
+    name: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+    },
+    eps: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+    },
+    identificationType: {
+        type: DataTypes.ENUM('CC', 'CE'),
+        allowNull: false,
+    },
+    identification: {
         type: DataTypes.STRING(50),
         allowNull: false,
         unique: true,
     },
-    Last_Name: {
-        type: DataTypes.STRING(50),
-        allowNull: false,
-    },
-    Email: {
-        type: DataTypes.STRING(50),
+    email: {
+        type: DataTypes.STRING(100),
         allowNull: false,
         unique: true,
     },
-    Password: {
+    cellphone: {
         type: DataTypes.STRING(50),
         allowNull: false,
     },
-    Birth_Date: {
-        type: DataTypes.DATE,
+    address: {
+        type: DataTypes.STRING(100),
         allowNull: false,
     },
-    Status: {
-        type: DataTypes.BOOLEAN,
+    password: {
+        type: DataTypes.STRING(150),
         allowNull: false,
     },
+    status: {
+        type: DataTypes.BOOLEAN(),
+        allowNull: false,
+        defaultValue: true
+    }
 }, {
     tableName: 'Customers', 
     timestamps: false,
--- src/repositories/Customer_Repository.js
+++ src/repositories/Customer_Repository.js
@@ -12,15 +12,23 @@ export class CustomerRepository{
         return await Customers.findByPk(id);
     }
 
+    async getCustomerByIdentification(id) {
+        return await Customers.findOne({ where: { identification: id } });
+    }
+
+    async getCustomerByEmail(email) {
+        return await Customers.findOne({ where: { email: email } });
+    }
+
     async createCustomer(data) {
         return await Customers.create(data);
     }
 
     async updateCustomer(id, data) {
-        return await Customers.update(data, { where: { id_customer: id } });
+        return await Customers.update(data, { where: { idCustomer: id } });
     }
 
     async deleteCustomer(id) {
-        return await Customers.destroy({ where: { id_customer: id } });
+        return await Customers.destroy({ where: { idCustomer: id } });
     }
 }
