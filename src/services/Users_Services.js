680662b016da6137be6e0fe97e41dbca82774123
juanpa0617 <juandev0617@gmail.com>
2025-03-05 08:23:02 -0500

    resolviendo  conflictos

--- src/services/Users_Services.js
+++ src/services/Users_Services.js
@@ -29,3 +29,5 @@ export const changeStatusUserService = async (id,State) => {
   return await changeStatusUser(id,State)
 }
 export const deleteUserService = async (id) => await deleteUser(id);
+
+//cambios para git
