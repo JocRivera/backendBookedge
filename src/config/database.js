590dac4c7c8498111c96351e4c353022da6c0125
juanpa0617 <juandev0617@gmail.com>
2025-06-09 10:17:28 -0500

    Config para despliegue

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,25 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
 
-const database = new Sequelize(
-  process.env.DB_NAME || "bookedge",
-  process.env.DB_USER || "root",
-  process.env.DB_PASSWORD || "",
+// const database = new Sequelize(
+//   process.env.DB_NAME || "bookedge",
+//   process.env.DB_USER || "root",
+//   process.env.DB_PASSWORD || "",
+//   {
+//     host: process.env.DB_HOST || "localhost",
+//     dialect: "mysql",
+//     logging: false,
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000,
+//     },
+//   }
+// );
+
+const database = new Sequelize(process.env.URL,
   {
-    host: process.env.DB_HOST || "localhost",
-    dialect: "mysql",
     logging: false,
     pool: {
       max: 5,
