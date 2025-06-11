ea6fc5ad925af3fb2fce9e9a1e87bf2ada6f9e71
JocRivera <jose_mrivera@soy.sena.edu.co>
2025-06-10 21:24:52 -0500

    localhost

--- src/config/database.js
+++ src/config/database.js
@@ -1,25 +1,13 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
 
-// const database = new Sequelize(
-//   process.env.DB_NAME || "bookedge",
-//   process.env.DB_USER || "root",
-//   process.env.DB_PASSWORD || "",
-//   {
-//     host: process.env.DB_HOST || "localhost",
-//     dialect: "mysql",
-//     logging: false,
-//     pool: {
-//       max: 5,
-//       min: 0,
-//       acquire: 30000,
-//       idle: 10000,
-//     },
-//   }
-// );
-
-const database = new Sequelize(process.env.URL,
+const database = new Sequelize(
+  process.env.DB_NAME || "bookedge",
+  process.env.DB_USER || "root",
+  process.env.DB_PASSWORD || "",
   {
+    host: process.env.DB_HOST || "localhost",
+    dialect: "mysql",
     logging: false,
     pool: {
       max: 5,
@@ -29,6 +17,18 @@ const database = new Sequelize(process.env.URL,
     },
   }
 );
+
+// const database = new Sequelize(process.env.URL,
+//   {
+//     logging: false,
+//     pool: {
+//       max: 5,
+//       min: 0,
+//       acquire: 30000,
+//       idle: 10000,
+//     },
+//   }
+// );
 
 //nada
 export { database };
